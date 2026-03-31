e829592152ff2fd13f92c4cefeaf8baa1ff08a0b
yarcevaed <yarcevaed@yandex.ru>
2026-03-31 09:10:19 +0300

    Factory.

--- Factory.h
+++ Factory.h
@@ -1,1 +1,11 @@
+#pragma once
+#include <vector>
+#include <string>
+#include "Employee.h"
+#include "Project.h"
 
+class StaffFactory 
+{
+public:
+    static vector<Employee*> makeStaff(const string& filename, vector<Project*>& projects);
+};
