29f6923c201f178e1eb17423c6bf39e147cb7fed
Your Name <you@example.com>
2026-03-31 09:03:12 +0300

    Engineer

--- Engineer.cpp
+++ Engineer.cpp
@@ -1,1 +1,94 @@
 #include "Engineer.h"
+#include <iostream>
+using namespace std;
+
+Engineer::Engineer(int id, const string& name, double rate, Project* proj)
+    : Personal(id, name, rate, proj)
+{}
+
+double Engineer::calcBudgetPart() const
+{
+    if (project)
+    {
+        return project->getBudget() * 0.01;
+    }
+    return 0;
+}
+
+double Engineer::calcProAdditions() const
+{
+    return 0;
+}
+
+void Engineer::calc()
+{
+    payment = calcBase() + calcBonus() + calcBudgetPart() + calcProAdditions();
+}
+
+string Engineer::getPosition() const
+{
+    return "Engineer";
+}
+
+Programmer::Programmer(int id, const string& name, double rate, Project* proj)
+    : Engineer(id, name, rate, proj), earlyBonus(0)
+{}
+
+void Programmer::setEarlyBonus(double bonus)
+{
+    earlyBonus = bonus;
+}
+
+double Programmer::calcProAdditions() const
+{
+    return earlyBonus;
+}
+
+string Programmer::getPosition() const
+{
+    return "Programmer";
+}
+
+Tester::Tester(int id, const string& name, double rate, Project* proj)
+    : Engineer(id, name, rate, proj), foundBugs(0), bonusPerBug(0)
+{}
+
+void Tester::setBugs(int bugs, double perBug)
+{
+    foundBugs = bugs;
+    bonusPerBug = perBug;
+}
+
+double Tester::calcProAdditions() const
+{
+    return foundBugs * bonusPerBug;
+}
+
+string Tester::getPosition() const
+{
+    return "Tester";
+}
+
+TeamLeader::TeamLeader(int id, const string& name, double rate, Project* proj)
+    : Programmer(id, name, rate, proj), subordinates(0) 
+{}
+
+void TeamLeader::setSubordinates(int count) {
+    subordinates = count;
+}
+
+double TeamLeader::calcHeading() const
+{
+    return subordinates * 500;
+}
+
+void TeamLeader::calc()
+{
+    Programmer::calc();
+    payment += calcHeading();
+}
+
+string TeamLeader::getPosition() const
+{
+    return "TeamLeader";
+}
--- Engineer.h
+++ Engineer.h
@@ -1,21 +1,61 @@
 #pragma once
+#include <iostream>
 #include "Personal.h"
-class Engineer : Personal
+#include "Interfaces.h"
+using namespace std;
+
+class Engineer : public Personal, public IProjectBudget
 {
-	
+public:
+
+    Engineer(int id, const string& name, double rate, Project* proj);
+    virtual ~Engineer() {}
+
+    virtual double calcBudgetPart() const override;
+    virtual double calcProAdditions() const override;
+
+    virtual void calc() override;
+    virtual string getPosition() const override;
 };
 
-class Tester : Engineer
+class Programmer : public Engineer
 {
+private:
 
+    double earlyBonus;
+
+public:
+
+    Programmer(int id, const string& name, double rate, Project* proj);
+    void setEarlyBonus(double bonus);
+    virtual double calcProAdditions() const override;
+    virtual string getPosition() const override;
 };
 
-class Programmer : Engineer
+class Tester : public Engineer
 {
+private:
 
+    int foundBugs;
+    double bonusPerBug;
+public:
+
+    Tester(int id, const string& name, double rate, Project* proj);
+    void setBugs(int bugs, double perBug);
+    virtual double calcProAdditions() const override;
+    virtual string getPosition() const override;
 };
 
-class TeamLeader : Programmer
+class TeamLeader : public Programmer, public IHeading
 {
+private:
 
+    int subordinates;
+public:
+
+    TeamLeader(int id, const string& name, double rate, Project* proj);
+    void setSubordinates(int count);
+    virtual double calcHeading() const override;
+    virtual void calc() override;
+    virtual string getPosition() const override;
 };
