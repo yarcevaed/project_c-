f946a3664bc3856a956479101c8972945e83a9ec
Your Name <you@example.com>
2026-03-31 09:12:28 +0300

    Interfaces

--- Interfaces.h
+++ Interfaces.h
@@ -1,37 +1,29 @@
 #pragma once
 class IWorkBaseTime
 {
+
 public:
-	int calcBase(int salary, int worktime) // метод расчета зарплаты исходя из оклада (за час) и отработанных часов
-	{
-		return salary * worktime;
-	}
-	virtual int calcBonus() //дополнительные выплаты
-	{
 
-	}
-
+    virtual ~IWorkBaseTime() {}
+    virtual double calcBase() const = 0;
+    virtual double calcBonus() const = 0;
 };
 
 class IProjectBudget
 {
+
 public:
-	int calcBudgetPart(float part, int budget) //расчет выплат из бюджета проекта
-	{
 
-	}
-	int calcProAdditions() //расчет *бонусных* выплат
-	{
-
-	}
-
+    virtual ~IProjectBudget() {}
+    virtual double calcBudgetPart() const = 0;
+    virtual double calcProAdditions() const = 0;
 };
 
 class IHeading
 {
-public: 
-	int calcHeads() //расчет оплаты исходя из руководства (количество подчиненных)
-	{
 
-	}
+public:
+
+    virtual ~IHeading() {}
+    virtual double calcHeading() const = 0;
 };
