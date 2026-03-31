7e88ab4f95dade7993adb3795bec60a147cd0ce5
Your Name <you@example.com>
2026-03-31 09:13:56 +0300

    Manager

--- Manager.cpp
+++ Manager.cpp
@@ -1,1 +1,74 @@
-#include "SeniorManager.h"
+
+#include "Manager.h"
+#include <iostream>
+using namespace std;
+
+ProjectManager::ProjectManager(int id, const string& name, double rate, Project* proj)
+    : Employee(id, name, rate, proj), subordinates(0)
+{
+
+}
+
+void ProjectManager::setSubordinates(int count)
+{
+    subordinates = count;
+}
+
+double ProjectManager::calcBudgetPart() const
+{
+    if (project)
+    {
+        return project->getBudget() * 0.05;
+    }
+    return 0;
+}
+
+double ProjectManager::calcProAdditions() const
+{
+    return 0;
+}
+
+double ProjectManager::calcHeading() const
+{
+    return subordinates * 300;
+}
+
+void ProjectManager::calc() {
+    payment = rate + calcBudgetPart() + calcHeading();
+}
+
+string ProjectManager::getPosition() const
+{
+    return "ProjectManager";
+}
+
+SeniorManager::SeniorManager(int id, const string& name, double rate, const vector<Project*>& projects)
+    : ProjectManager(id, name, rate, nullptr), allProjects(projects)
+{
+
+}
+
+void SeniorManager::setSubordinates(int count)
+{
+    ProjectManager::setSubordinates(count);
+}
+
+double SeniorManager::calcBudgetPart() const
+{
+    double total = 0;
+    for (auto proj : allProjects)
+    {
+        total += proj->getBudget() * 0.02;
+    }
+    return total;
+}
+
+void SeniorManager::calc()
+{
+    payment = rate + calcBudgetPart() + calcHeading();
+}
+
+string SeniorManager::getPosition() const
+{
+    return "SeniorManager";
+}
--- Manager.h
+++ Manager.h
@@ -1,11 +1,45 @@
+
 #pragma once
 #include "Employee.h"
-class ProjectManager : Employee
+#include "Interfaces.h"
+#include <vector>
+using namespace std;
+
+class ProjectManager : public Employee, public IProjectBudget, public IHeading
 {
 
+private:
+
+    int subordinates;
+
+public:
+
+    ProjectManager(int id, const string& name, double rate, Project* proj);
+    void setSubordinates(int count);
+
+    virtual double calcBudgetPart() const override;
+    virtual double calcProAdditions() const override;
+
+    virtual double calcHeading() const override;
+
+    virtual void calc() override;
+    virtual string getPosition() const override;
 };
 
-class SeniorManager : ProjectManager
+class SeniorManager : public ProjectManager
 {
 
+private:
+
+    vector<Project*> allProjects;
+
+public:
+
+    SeniorManager(int id, const string& name, double rate, const vector<Project*>& projects);
+    void setSubordinates(int count);
+
+    virtual double calcBudgetPart() const override;
+
+    virtual void calc() override;
+    virtual string getPosition() const override;
 };
